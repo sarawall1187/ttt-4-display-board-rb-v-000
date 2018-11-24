574366b814883798d02be4b9e89e1e6d754bfb08
Sara Walker <sarawall1187@gmail.com>
2018-11-24 20:17:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,15 @@
 def display_board(board)
+<<<<<<< HEAD
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+>>>>>>> f453a725b238f770a3b31fe53b621ce9036690d1
   end
